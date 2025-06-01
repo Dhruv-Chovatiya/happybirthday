f9977c8cdda51b7484bb9e29d50dae881b44083c
Dhruv Chovatiya <85601704+Dhruv-Chovatiya@users.noreply.github.com>
2025-06-01 23:11:07 +0530

    Update main.js

--- script/main.js
+++ script/main.js
@@ -115,8 +115,6 @@ const animationTimeline = () => {
     "+=1")
     .from(".idea-1", 0.7, ideaTextTrans)
     .to(".idea-1", 0.7, ideaTextTransLeave, "+=2.5")
-    .from(".idea-1.5", 0.7, ideaTextTrans)
-    .to(".idea-1.5", 0.7, ideaTextTransLeave, "+=2.5")
     .from(".idea-2", 0.7, ideaTextTrans)
     .to(".idea-2", 0.7, ideaTextTransLeave, "+=2.5")
     .from(".idea-3", 0.7, ideaTextTrans)
